0c674b4c8f45c582380ec86b789cb1cf13570ef2
Jefferson <jefferson.hsieh@aya.yale.edu>
2023-12-03 14:31:02 -0500

    test exploration code

--- visualnav-transformer/deployment/src/explore.sh
+++ visualnav-transformer/deployment/src/explore.sh
@@ -16,21 +16,24 @@ tmux selectp -t 0    # go back to the first pane
 
 # Run the roslaunch command in the first pane
 tmux select-pane -t 0
-tmux send-keys "roslaunch vint_locobot.launch" Enter
+tmux send-keys "source ~/Desktop/AirSim/ros/devel/setup.bash" Enter
+tmux send-keys "roslaunch airsim_ros_pkgs airsim_node.launch" Enter
 
 # Run the navigate.py script with command line args in the second pane
 tmux select-pane -t 1
-# tmux send-keys "conda activate vint_deployment" Enter
-tmux send-keys "python explore.py $@" Enter
+tmux send-keys "conda activate t2r_vint" Enter
+tmux send-keys "python explore_airsim.py $@" Enter
 
 # Run the teleop.py script in the third pane
 tmux select-pane -t 2
-# tmux send-keys "conda activate vint_deployment" Enter
-tmux send-keys "python joy_teleop.py" Enter
+# tmux send-keys "source ~/Desktop/AirSim/ros/devel/setup.bash" Enter
+# tmux send-keys "roslaunch airsim_ros_pkgs position_controller_simple.launch"
+tmux send-keys "conda activate t2r_vint" Enter
+tmux send-keys "python keyboard_sim_controller.py"
 
 # Run the pd_controller.py script in the fourth pane
 tmux select-pane -t 3
-tmux send-keys "conda activate vint_deployment" Enter
+tmux send-keys "conda activate t2r_vint" Enter
 tmux send-keys "python pd_controller.py" Enter
 
 # Attach to the tmux session
