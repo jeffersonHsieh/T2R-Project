5eb9f3ec165b6298663139ef487898c5cce44796
Jefferson <jefferson.hsieh@aya.yale.edu>
2023-12-03 02:27:40 -0500

    interface with airsim to track trajectory issue: yaw not working, not sure if it's VINT or sim issue

--- visualnav-transformer/deployment/src/pd_controller.py
+++ visualnav-transformer/deployment/src/pd_controller.py
@@ -12,12 +12,21 @@ from topic_names import (WAYPOINT_TOPIC,
 from ros_data import ROSData
 from utils import clip_angle
 
+
+# AIRSIM control
+import airsim
+from airsim.types import YawMode
+
+
 # CONSTS
-CONFIG_PATH = "../config/robot.yaml"
+CONFIG_PATH = "../config/sim_drone.yaml"
 with open(CONFIG_PATH, "r") as f:
 	robot_config = yaml.safe_load(f)
 MAX_V = robot_config["max_v"]
+
+
 MAX_W = robot_config["max_w"]
+
 VEL_TOPIC = robot_config["vel_navi_topic"]
 DT = 1/robot_config["frame_rate"]
 RATE = 9
@@ -31,6 +40,12 @@ waypoint = ROSData(WAYPOINT_TIMEOUT, name="waypoint")
 reached_goal = False
 reverse_mode = False
 current_yaw = None
+
+client = airsim.MultirotorClient()
+client.confirmConnection()
+# client.reset()
+client.enableApiControl(True)
+client.takeoffAsync().join()
 
 def clip_angle(theta) -> float:
 	"""Clip angle to [-pi, pi]"""
@@ -65,7 +80,7 @@ def pd_controller(waypoint: np.ndarray) -> Tuple[float]:
 def callback_drive(waypoint_msg: Float32MultiArray):
 	"""Callback function for the waypoint subscriber"""
 	global vel_msg
-	print("seting waypoint")
+	# print("seting waypoint")
 	waypoint.set(waypoint_msg.data)
 	
 	
@@ -76,7 +91,7 @@ def callback_reached_goal(reached_goal_msg: Bool):
 
 
 def main():
-	global vel_msg, reverse_mode
+	global vel_msg, reverse_mode, client
 	rospy.init_node("PD_CONTROLLER", anonymous=False)
 	waypoint_sub = rospy.Subscriber(WAYPOINT_TOPIC, Float32MultiArray, callback_drive, queue_size=1)
 	reached_goal_sub = rospy.Subscriber(REACHED_GOAL_TOPIC, Bool, callback_reached_goal, queue_size=1)
@@ -86,16 +101,43 @@ def main():
 	while not rospy.is_shutdown():
 		vel_msg = Twist()
 		if reached_goal:
+			client.landAsync().join()
 			vel_out.publish(vel_msg)
 			print("Reached goal! Stopping...")
 			return
 		elif waypoint.is_valid(verbose=True):
 			v, w = pd_controller(waypoint.get())
+			# import pdb;pdb.set_trace()
 			if reverse_mode:
 				v *= -1
+			
+			print(f"publishing new vel: {v}, {w}")
+
+			# print(MAX_V, MAX_W)
+
+			# yaw = YawMode(is_rate=False, yaw_or_rate=w)
+			# client.moveByVelocityZBodyFrameAsync(v, 0, -3, 1,yaw_mode=yaw).join()
+
+			# # Get the state of the drone
+			state = client.getMultirotorState()
+
+			# Extract the orientation as a quaternion
+			orientation_q = state.kinematics_estimated.orientation
+
+			# Convert quaternion to Euler angles
+			orientation_euler = airsim.to_eularian_angles(orientation_q)
+
+			# Yaw is the third element in the Euler angles (roll, pitch, yaw)
+			current_yaw = orientation_euler[2]
+
+			yaw = YawMode(is_rate=False, yaw_or_rate=clip_angle(current_yaw + w))
+			vx = v * np.cos(current_yaw)
+			vy = v * np.sin(current_yaw)
+			client.moveByVelocityZAsync(vx, vy, -3, 1, yaw_mode=yaw).join()
+
 			vel_msg.linear.x = v
 			vel_msg.angular.z = w
-			print(f"publishing new vel: {v}, {w}")
+			
 		vel_out.publish(vel_msg)
 		rate.sleep()
 	
