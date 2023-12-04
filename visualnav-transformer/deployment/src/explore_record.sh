cde2b6e276a8a8dda46dcc74f6b45907dbd46db6
Jefferson <jefferson.hsieh@aya.yale.edu>
2023-12-03 19:46:06 -0500

    record more stuff

--- visualnav-transformer/deployment/src/explore_record.sh
+++ visualnav-transformer/deployment/src/explore_record.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
 
 # Create a new tmux session
-session_name="record_bag_$(date +%s)"
+session_name="vint_locobot_$(date +%s)"
 tmux new-session -d -s $session_name
 
-# Split the window into three panes
+# Split the window into four panes
+tmux selectp -t 0    # select the first (0) pane
+tmux splitw -h -p 50 # split it into two halves
 tmux selectp -t 0    # select the first (0) pane
 tmux splitw -v -p 50 # split it into two halves
-tmux selectp -t 0    # go back to the first pane
+
+tmux selectp -t 2    # select the new, second (2) pane
+tmux splitw -v -p 50 # split it into two halves
 tmux splitw -h -p 50 # split it into two halves
+tmux selectp -t 0    # go back to the first pane
 
 # Run the roslaunch command in the first pane
 tmux select-pane -t 0
 tmux send-keys "source ~/Desktop/AirSim/ros/devel/setup.bash" Enter
 tmux send-keys "roslaunch airsim_ros_pkgs airsim_node.launch" Enter
 
-# Run the teleop.py script in the second pane
+# Run the navigate.py script with command line args in the second pane
 tmux select-pane -t 1
 tmux send-keys "conda activate t2r_vint" Enter
-tmux send-keys "python keyboard_sim_controller.py" Enter
+# pass args except the first one (which is the name of the rosbag
+tmux send-keys "python explore_airsim.py ${@:2}" Enter
 
-# Change the directory to ../topomaps/bags and run the rosbag record command in the third pane
+# Run the teleop.py script in the third pane
 tmux select-pane -t 2
+# tmux send-keys "source ~/Desktop/AirSim/ros/devel/setup.bash" Enter
+# tmux send-keys "roslaunch airsim_ros_pkgs position_controller_simple.launch"
+tmux send-keys "conda activate t2r_vint" Enter
+tmux send-keys "python keyboard_sim_controller.py"
+
+# Run the pd_controller.py script in the fourth pane
+tmux select-pane -t 3
+tmux send-keys "conda activate t2r_vint" Enter
+tmux send-keys "python pd_controller.py" Enter
+
+
+tmux select-pane -t 4
 tmux send-keys "cd ../topomaps/bags" Enter
 tmux send-keys "rosbag record /airsim_node/SimpleFlight/front_center/Scene/compressed \
 /airsim_node/SimpleFlight/front_center/Scene/compressedDepth \
@@ -33,6 +51,7 @@ tmux send-keys "rosbag record /airsim_node/SimpleFlight/front_center/Scene/compr
 /airsim_node/SimpleFlight/left_camera/Scene/compressedDepth \
 /airsim_node/SimpleFlight/right_camera/Scene/compressedDepth \
 /airsim_node/SimpleFlight/right_camera/Scene/compressed -o $1" # change topic if necessary
+
 
 # Attach to the tmux session
 tmux -2 attach-session -t $session_name
